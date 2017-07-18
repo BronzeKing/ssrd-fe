916b9e9ca5365b02a112c3650fcaf15f614b3969
Evansy <skcy@vip.qq.com>
2017-07-18 23:13:47 +0800

    init ssrd-fe project

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    
+    // 自定义添加
+    /**
+     * "off" 或 0 - 关闭规则
+     * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出),
+     * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+     */
+
+    // 缩进设置 http://eslint.org/docs/rules/indent "SwitchCase" (默认：0) 强制 switch 语句中的 case 子句的缩进水平
+    'indent': ['error', 4, { "SwitchCase": 1 }],
+    //语句强制分号结尾
+    'semi': [2, 'always'],
+    // 禁止使用多个空格
+    'no-multi-spaces': 0,
+    // 禁止出现未使用过的变量
+    "no-unused-vars": 0,
+    // 强制在对象字面量的属性中键和值之间使用一致的间距
+    "key-spacing": 0,
+    // 禁用 console
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 1,
+    // 强制在圆括号内使用一致的空格
+    'space-in-parens': [ 2, 'never' ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,53 @@
+# ssrd-fe
+
+> ssrd fe project
+
+## 生成步骤
+
+``` bash
+# 安装依赖
+npm i
+
+# 运行开发环境 localhost:8090
+npm run dev
+
+# 运行生产编译
+npm run build
+```
+## 项目结构
+
+┌── src             		# main
+│    ├── api 				# API接口地址以及获取数据函数
+│    ├── assets     		# image source
+│    ├── components 		# components
+│    ├── configs      		# 该文件主要用来处理vue运行中所需js以及组件注册
+│    ├── enums      		# 枚举相关文件
+│    ├── filters      		# vue 全局filters
+│    ├── mixins      		# vue多页面共用mixins
+│    ├── pages      		# pages
+│    ├── routers      		# vue-router前端路由
+│    ├── scss  				# scss
+│    │    ├── font  		# iconfont
+│    │    ├── base  		# 基础的全局scss变量以及全局css
+│    │    ├── components  	# 组件相关样式，组件内单独引用
+│    │    └── pages   		# 页面相关样式，页面内单独引用
+│    │
+│    ├── settings      		# 全局设置
+│    ├── utils 				# 扩展相关函数以及封装
+│    ├── vuexs  	  		# vuex数据流管理相关
+│    │    └── modules   	# 按模块使用
+│    │
+│	 ├── App.vue  			# 入口
+│    └── main.js   			# 入口
+│
+├── node_modules    # 依赖
+├── static          # 静态文件
+├── dist            # 打包之后的文件
+├── .babelrc        # babel config
+├── .editorconfig	# 通用编辑器配置
+├── .gitignore		# git忽略提交配置
+├── .eslintignore   # 暂时添加了src/*,去掉了监控
+├── .eslintrc.js    # eslint配置
+├── .postcssrc.js   # postcss配置
+├── index.html      # index入口文件
+└── package.json    # package info
