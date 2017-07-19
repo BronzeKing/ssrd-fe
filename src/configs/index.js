ca98c0066e59edd5ad7bb4cb4cac7cf5e43c3b81
Evansy <skcy@vip.qq.com>
2017-07-20 00:17:35 +0800

    * 挂载枚举到Vue原型上 * 修改写法全局注册所有filters

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,13 +1,19 @@
 /**
  * 该文件主要用来处理vue运行中所需js以及组件注册，全局样式等
  */
-import Vue from 'vue';
-import { FormartPrice } from 'filters';
-import { LineThrough, Hello } from 'components';
+import Vue                      from 'vue';
+import * as filters             from 'filters';
+import {LineThrough, Hello}     from 'components';
+import * as enums               from 'enums';
 
 // 注册vue filters;
-Vue.filter('FormartPrice', FormartPrice);
+filters.keys.forEach(name => {
+    Vue.filter(name, filters[name]);
+});
 
 // 注册全局组件
 Vue.component('LineThrough', LineThrough);
 Vue.component('Hello', Hello);
+
+// 原型挂在常用变量
+Vue.prototype.$enums = enums;
