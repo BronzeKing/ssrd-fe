dead557f29b922a63139c680c9ceb92a213f741c
leon <ming5964471402wu@qq.com>
2017-07-20 09:54:16 +0800

    测试 提交

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,7 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
-        
+        margin-top: 50px;
     }
     
     @import 'scss/common.scss';
