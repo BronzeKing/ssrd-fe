2b88d62a7514fd2e04e3cbdf7ddd6f03837e7e3f
Evansy <skcy@vip.qq.com>
2017-08-07 23:36:10 +0800

    * 添加pug语法 * 初步添加公共header

--- src/pages/views/header.vue
+++ src/pages/views/header.vue
@@ -1,8 +1,16 @@
-<template>
-    <div class="page-header">
-        <div class="container">
-        </div>
-    </div>
+<template lang="pug">
+    .page-header
+        .header-bar
+            .container.flex
+                span 欢迎来到深圳市盛世润达智能科技有限公司官方网站
+                span
+                    a.user-option-item.active(href="javascript: void(0);") [ 登陆 ]
+                    a.user-option-item(href="javascript: void(0);") [ 注册 ]
+                    a.user-option-item(href="javascript: void(0);") 会员中心
+                    a.user-option-item(href="javascript: void(0);") 购物车
+                    a.user-option-item(href="javascript: void(0);") 快捷下单
+        .container.flex
+            img(src="~assets/logo.png")
 </template>
 <script>
     export default {
@@ -15,5 +23,5 @@
     };
 </script>
 <style lang="scss">
-    @import "~scss/pages/home";
+    @import "~scss/pages/views/header";
 </style>
