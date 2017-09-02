b6f64a1b4736383d28ae2ec71464e5d97ac554db
drinks <drinksober@foxmail.com>
2017-09-02 22:12:44 +0800

    fix: fix url config

--- src/apis/api-urls.js
+++ src/apis/api-urls.js
@@ -6,8 +6,8 @@ import { apiUrl } from 'settings';
 
 export default {
     // 登录注册相关接口
-    login:      `${apiUrl}/Merchant/login`,                                 // 登录接口
-    register:   `${apiUrl}/Merchant/Register`,                              // 注册接口
+    login:      `${apiUrl}/token`,                                 // 登录接口
+    register:   `${apiUrl}/signup`,                              // 注册接口
 
     // 新闻
     news:       `${apiUrl}/news`                                            // 获取新闻
