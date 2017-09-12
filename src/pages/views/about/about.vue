3a5d1220aeab81f8435bd90a2998d8b1ee147b83
Leon522 <ming5964471402wu@qq.com>
2017-09-12 23:11:04 +0800

    关于我们页面路由配置

--- src/pages/views/about/about.vue
+++ src/pages/views/about/about.vue
@@ -1,5 +1,16 @@
 <template lang="pug">
-    
+    .page-about.container
+        .about-banner
+            img(src="~assets/banner.png")
+        .about-content
+            .about-menu
+            .about-info
+                .breadcrumb
+                    el-breadcrumb
+                        el-breadcrumb-item 首页
+                        el-breadcrumb-item 关于我们
+                        el-breadcrumb-item 招贤纳士
+                
 </template>
 <script>
     // 关于我们页面
@@ -7,3 +18,6 @@
         name: 'about'
     };
 </script>
+<style lang="scss">
+    @import "~scss/pages/about";
+</style>
