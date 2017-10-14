306c05693edf8190997e8662ca2d29258070bcb3
huawu <ming5964471402wu@qq.com>
2017-10-14 23:23:39 +0800

    增加系统展示

---
+++ src/components/menu/menu.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+    .about-menu
+        .menu-title
+            .line-title
+            .title-info
+                p.f18 {{menuData.menuInfo.name}}
+                p.f12 {{menuData.menuInfo.englishName}}
+        .menu-wrap
+            ul
+                li(v-for="(item,index) in menuData.menuList" :class="{active: menuData.activeTab===index}" @click="linkTo(index)")
+                    a
+                        span {{item}}
+                        i.iconfont.icon-arrow-right
+</template>
+<script>
+export default {
+    name: 'menuBox',
+    data () {
+        return {
+        };
+    },
+    props: {
+        menuData: {
+            type: Object
+        }
+    },
+    mounted () {
+        this.activeTab = Number(this.$route.query.id);
+    },
+    methods: {
+        linkTo (data) {
+            this.$emit('linkTo', data);
+        }
+    }
+};
+</script>
+<style lang="scss">
+    @import "./menu";
+</style>
