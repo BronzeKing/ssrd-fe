51f8197b26b98348c673da6cb247ac88c1947ad2
Evansy <skcy@vip.qq.com>
2017-10-24 22:23:46 +0800

    * 优化代码

--- src/apis/resource.js
+++ src/apis/resource.js
@@ -21,6 +21,7 @@ class Resource {
         this.m = {};
         this.rules = {};
         this.errors = {};
+
         // list方法的 table的查询参数以及分页控件、搜索条件 table=t, model=m
         this.t = {
             search: '',
@@ -29,6 +30,8 @@ class Resource {
             RecordCount: 0,
             Records: []
         };
+
+        // 如果model不为空，存储model及rule，并清空
         if (Model) {
             this.m = Model.data;
             this.rules = Model.rules;
@@ -47,11 +50,7 @@ class Resource {
     resetModel (obj) {
         // obj 传入的是 {job: 'test'}  这样的，如果不传obj, 默认把model都置为''
         Object.keys(this.m).forEach(key => {
-            if (key in obj) {
-                this.m[key] = obj[key];
-            } else {
-                this.m[key] = '';
-            }
+            this.m[key] = key in obj ? obj[key] : '';
         });
     }
 
@@ -66,6 +65,7 @@ class Resource {
         // obj 传入的是 {job: 'test'}  这样的，如果不传obj, 默认把model作为formData
         const data = this.m;
         const form = new FormData();
+
         Object.keys(obj).forEach(key => {
             data[key] = obj[key];
         });
@@ -81,6 +81,7 @@ class Resource {
     request (body = {}, config, action) {
         const method = _methodMap[action];
         let url = this.url;
+        let response;
 
         // retrieve, update,  delete 方法path中都带有id
         if (!['list', 'create'].includes(action)) {
@@ -91,8 +92,10 @@ class Resource {
 
         // retrieve, list方法 为get方法
         body = ['retrieve', 'list'].includes(action) ? { params: body } : this.formData(body);
-        let response = method(url, body, config);
-        let that = this;
+
+        // 发起请求
+        response = method(url, body, config);
+
         // 把上一次请求产生的错误清除
         this.resetErrors();
 
@@ -106,14 +109,14 @@ class Resource {
     }
 
     // 获取资源列表
-    list (params, config) {
-        params = params || {};
+    list (params = {}, config) {
         // 默认把this.t里的search, pageIndex, pageSize参数传到list方法里
         ['search', 'pageIndex', 'pageSize'].forEach(k => {
             if (this.t[k]) {
                 params[k] = this.t[k];
             };
         });
+
         return this.request(params, config, 'list').then(r => {
             this.t['RecordCount'] = r['RecordCount'];
             this.t['Records'] = r['Records'];
