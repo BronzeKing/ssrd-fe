0588557b3e6a52f898db07b0065aaee4b84482a2
mum5 <mm@mum5.cn>
2017-11-05 12:29:08 +0800

    add: 增加全局声明Payload， 用于存放基本http数据类型

--- src/vue.shims.d.ts
+++ src/vue.shims.d.ts
@@ -3,6 +3,25 @@ declare module "*.vue" {
   export default Vue
 }
 
+/**
+* @说明 在vue文件和ts文件声明Payload，用于存放http的基本数据类型
+*/
+declare module "src/pages/*.vue" {
+  global {
+    interface Payload {
+        [key: string]: any;
+    }
+  }
+}
+
+declare module "src/pages/*.ts" {
+  global {
+    interface Payload {
+        [key: string]: any;
+    }
+  }
+}
+
 declare const process: {
     env: {
         NODE_ENV: string
