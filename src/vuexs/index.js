bd9101cc004d4784e0f9bc8cc0325becd220ee2d
mum5 <mm@mum5.cn>
2017-11-05 14:58:48 +0800

    refactor: 重构所有vuex到ts 并且删除旧的resource和model \n

--- src/vuexs/index.js
+++ src/vuexs/index.js
@@ -1,25 +1,25 @@
 /**
  * vuex相关的处理
  */
-import Vue from 'vue';
-import Vuex from 'vuex';
-import createLogger from 'vuex/dist/logger';
+import Vue from "vue";
+import Vuex from "vuex";
+import createLogger from "vuex/dist/logger";
 
-import global           from './modules/global';
-import home             from './modules/home';
-import user             from './modules/user';
+import global from "./modules/global";
+import home from "./modules/home";
+import user from "./modules/user";
 
 Vue.use(Vuex);
 
 // 是否开启vuex logger。
-const debug = process.env.NODE_ENV !== 'production';
+const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
-    strict: debug,
-    plugins: debug ? [createLogger()] : [],
-    modules: {
-        global,
-        user,
-        home
-    }
+  strict: debug,
+  plugins: debug ? [createLogger({})] : [],
+  modules: {
+    global,
+    user,
+    home
+  }
 });
