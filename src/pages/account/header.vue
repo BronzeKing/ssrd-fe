531e665bc9051f7d63ad2bf7a2d7453f883b42ec
Evansy <skcy@vip.qq.com>
2017-11-07 21:05:19 +0800

    * 修复路由跳转问题

--- src/pages/account/header.vue
+++ src/pages/account/header.vue
@@ -6,10 +6,10 @@
                 i.user-header-line
                 h2 个人中心
             nav
-                router-link(class="user-header-link active" to="home") 首页
-                router-link(class="user-header-link" to="home") 账户设置
-                router-link(class="user-header-link" to="home") 消息
-                router-link(class="user-header-link" to="home") 购物车
+                router-link(class="user-header-link active" to="/home") 首页
+                router-link(class="user-header-link" to="/home") 账户设置
+                router-link(class="user-header-link" to="/home") 消息
+                router-link(class="user-header-link" to="/home") 购物车
 </template>
 
 <script lang="ts">
