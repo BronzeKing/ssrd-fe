5479b894a270e8b541c2b7fb42e43144dd4ef5f1
Evan <skcy@vip.qq.com>
2017-12-11 00:07:13 +0800

    * 调整商品列表样式

--- src/pages/views/product/components/product-list.vue
+++ src/pages/views/product/components/product-list.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
-    div
+    div.page-product-list
         img(src="~assets/product-show.png")
         .product-content.mt15
-            div(v-for="item in Product.t.Records" :key="item.id")
-                img(:src="item.background")
+            div.product-list-item(v-for="item in Product.t.Records" :key="item.id")
                 router-link(:to="{name: 'productDetail', params: {id: item.id} }")
-                    p {{item.name}}
+                    div.product-image-box
+                        img.product-image(:src="item.background")
+                    p.product-name {{item.name}}
         el-pagination.mt20.tc(@current-change="Product.list" :page-size="Product.t.pageSize" layout="prev, pager, next, jumper" :total="Product.t.PageCount" :current-page.sync="Product.t.pageIndex")
         
     
@@ -31,5 +32,5 @@ export default class ProductListView extends Vue
 </script>
 <style lang="scss">
 @import "~scss/pages/views/product/product";
-    
+@import "~scss/pages/views/product/product-list";
 </style>
