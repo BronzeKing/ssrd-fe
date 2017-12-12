1f7cc05eeffd40153a98492115f54ad2289cd997
mum5 <mum5>
2017-12-12 15:06:27 +0800

    remove dll

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -16,156 +16,149 @@ const ParallelUglifyPlugin = require("webpack-parallel-uglify-plugin");
 let ImageminPlugin = require("imagemin-webpack-plugin").default;
 
 var qiniuPlugin = new QiniuPlugin({
-  ACCESS_KEY: "NtO1B6ILEpI33oQq1NpiCSAzpgaASsrmmJzNQsNG",
-  SECRET_KEY: "Z2ztsscMNJZVZiZpDc8UCZnChMR46ncGQpuIrld9",
-  bucket: "mumumu",
-  path: "[hash]"
+    ACCESS_KEY: "NtO1B6ILEpI33oQq1NpiCSAzpgaASsrmmJzNQsNG",
+    SECRET_KEY: "Z2ztsscMNJZVZiZpDc8UCZnChMR46ncGQpuIrld9",
+    bucket: "mumumu",
+    path: "[hash]"
 });
 
-var env =
-  process.env.NODE_ENV === "testing"
-    ? require("../config/test.env")
-    : config.build.env;
+var env = process.env.NODE_ENV === "testing" ? require("../config/test.env") : config.build.env;
 
 var webpackConfig = merge(baseWebpackConfig, {
-  module: {
-    rules: utils.styleLoaders({
-      sourceMap: config.build.productionSourceMap,
-      extract: true
-    })
-  },
-  devtool: config.build.productionSourceMap ? "#source-map" : false,
-  output: {
-    path: config.build.assetsRoot,
-    filename: utils.assetsPath("js/[name].js"),
-    //chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
-    publicPath: "https://static.mum5.cn/[hash]/"
-  },
-  plugins: [
-    // http://vuejs.github.io/vue-loader/en/workflow/production.html
-    qiniuPlugin,
-    new webpack.DefinePlugin({
-      "process.env": env
-    }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //     drop_debugger: true,    // 去掉代码中所有debugger代码
-    //     drop_console: true      // 去掉代码中所有console代码
-    //   },
-    //   sourceMap: true
-    // }),
-    new ParallelUglifyPlugin({
-      cacheDir: ".cache/",
-      uglifyJS: {
-        output: {
-          comments: false,
-          beautify: false
-        },
-        compress: {
-          warnings: false,
-          drop_debugger: true, // 去掉代码中所有debugger代码
-          drop_console: true
-        }
-      }
-    }),
-    // extract css into its own file
-    new ExtractTextPlugin({
-      filename: utils.assetsPath("css/[name].[contenthash].css")
-    }),
-    // Compress extracted CSS. We are using this plugin so that possible
-    // duplicated CSS from different components can be deduped.
-    new OptimizeCSSPlugin({
-      cssProcessorOptions: {
-        safe: true
-      }
-    }),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename:
-        process.env.NODE_ENV === "testing" ? "index.html" : config.build.index,
-      template: "index2.html",
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: "dependency"
-    }),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks: function(module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(path.join(__dirname, "../node_modules")) === 0
-        );
-      }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "manifest",
-      chunks: ["vendor"]
-    }),
+    module: {
+        rules: utils.styleLoaders({
+            sourceMap: config.build.productionSourceMap,
+            extract: true
+        })
+    },
+    devtool: config.build.productionSourceMap ? "#source-map" : false,
+    output: {
+        path: config.build.assetsRoot,
+        filename: utils.assetsPath("js/[name].js"),
+        //chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
+        publicPath: "https://static.mum5.cn/[hash]/"
+    },
+    plugins: [
+        // http://vuejs.github.io/vue-loader/en/workflow/production.html
+        qiniuPlugin,
+        new webpack.DefinePlugin({
+            "process.env": env
+        }),
+        // new webpack.optimize.UglifyJsPlugin({
+        //   compress: {
+        //     warnings: false,
+        //     drop_debugger: true,    // 去掉代码中所有debugger代码
+        //     drop_console: true      // 去掉代码中所有console代码
+        //   },
+        //   sourceMap: true
+        // }),
+        new ParallelUglifyPlugin({
+            cacheDir: ".cache/",
+            uglifyJS: {
+                output: {
+                    comments: false,
+                    beautify: false
+                },
+                compress: {
+                    warnings: false,
+                    drop_debugger: true, // 去掉代码中所有debugger代码
+                    drop_console: true
+                }
+            }
+        }),
+        // extract css into its own file
+        new ExtractTextPlugin({
+            filename: utils.assetsPath("css/[name].[contenthash].css")
+        }),
+        // Compress extracted CSS. We are using this plugin so that possible
+        // duplicated CSS from different components can be deduped.
+        new OptimizeCSSPlugin({
+            cssProcessorOptions: {
+                safe: true
+            }
+        }),
+        // generate dist index.html with correct asset hash for caching.
+        // you can customize output by editing /index.html
+        // see https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: process.env.NODE_ENV === "testing" ? "index.html" : config.build.index,
+            template: "index2.html",
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+                // more options:
+                // https://github.com/kangax/html-minifier#options-quick-reference
+            },
+            // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+            chunksSortMode: "dependency"
+        }),
+        // split vendor js into its own file
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            minChunks: function(module, count) {
+                // any required modules inside node_modules are extracted to vendor
+                return (
+                    module.resource &&
+                    /\.js$/.test(module.resource) &&
+                    module.resource.indexOf(path.join(__dirname, "../node_modules")) === 0
+                );
+            }
+        }),
+        // extract webpack runtime and module manifest to its own file in order to
+        // prevent vendor hash from being updated whenever app bundle is updated
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "manifest",
+            chunks: ["vendor"]
+        }),
 
-    // Make sure that the plugin is after any plugins that add images
-    // 图片体积压缩插件
-    new ImageminPlugin({
-      disable: process.env.NODE_ENV !== "production", // Disable during development
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      minFileSize: 70000, // 大于70k的会被压缩
-      jpegtran: { progressive: true },
-      pngquant: {
-        quality: "80-90" // 压缩质量
-      }
-    }),
+        // Make sure that the plugin is after any plugins that add images
+        // 图片体积压缩插件
+        new ImageminPlugin({
+            disable: process.env.NODE_ENV !== "production", // Disable during development
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            minFileSize: 70000, // 大于70k的会被压缩
+            jpegtran: { progressive: true },
+            pngquant: {
+                quality: "80-90" // 压缩质量
+            }
+        }),
 
-    // webpack.DllPlugin.js
-    new webpack.DllReferencePlugin({
-      context: path.resolve(__dirname, ".."),
-      manifest: require("./vendor-manifest.json")
-    }),
+        // webpack.DllPlugin.js
+        // new webpack.DllReferencePlugin({
+        //   context: path.resolve(__dirname, ".."),
+        //   manifest: require("./vendor-manifest.json")
+        // }),
 
-    // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, "../static"),
-        to: config.build.assetsSubDirectory,
-        ignore: [".*"]
-      }
-    ])
-  ]
+        // copy custom static assets
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, "../static"),
+                to: config.build.assetsSubDirectory,
+                ignore: [".*"]
+            }
+        ])
+    ]
 });
 
 if (config.build.productionGzip) {
-  var CompressionWebpackPlugin = require("compression-webpack-plugin");
+    var CompressionWebpackPlugin = require("compression-webpack-plugin");
 
-  webpackConfig.plugins.push(
-    new CompressionWebpackPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: new RegExp(
-        "\\.(" + config.build.productionGzipExtensions.join("|") + ")$"
-      ),
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  );
+    webpackConfig.plugins.push(
+        new CompressionWebpackPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: new RegExp("\\.(" + config.build.productionGzipExtensions.join("|") + ")$"),
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    );
 }
 
 if (config.build.bundleAnalyzerReport) {
-  var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-    .BundleAnalyzerPlugin;
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin());
+    var BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+    webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 }
 
 module.exports = webpackConfig;
